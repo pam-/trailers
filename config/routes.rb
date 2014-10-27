bc181306796e6f25375ccfcba101d92dfd0e4a63
pam- <pam.assogba@gmail.com>
2014-10-27 10:30:16 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   
   root 'movies#index'
+
+  devise_scope :user do
+    get '/sign_up', to: 'devise/registrations#new'
+    get '/sign_in', to: 'devise/sessions#new'
+    delete 'sign_out', to: 'devise/sessions#destroy'
+  end 
+
+  
 end
